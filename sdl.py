4f669f178aa04515c27d87219cfe4682b45be087
sreekumart <nair.sreekumar.t@gmail.com>
2020-02-23 12:18:34 +0530

    driver module
    initial commit

---
+++ sdl.py
@@ -0,0 +1,17 @@
+from sdl import SoundCloudDl
+
+
+def download(url, download_path, client_id):
+    s = SoundCloudDl(url, download_path, client_id)
+    s.get_source_url()
+    s.get_playlist_url()
+    s.get_playlist()
+    s.get_files()
+
+
+if __name__ == "__main__":
+    url = input("Enter the URL")
+    download_path = input("enter the download path (optional)")
+    client_id = input("enter the client id (optional)")
+    download(url, download_path, client_id)
+
